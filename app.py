2de96e80b7c68d13afef8f402b3e93ed8dd02e00
Shruti Balan <77207245+shrutibalan4591@users.noreply.github.com>
2022-10-29 16:14:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,70 +3,13 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import bz2file as bz2
 
-
-# Method for encoding gender value
-def gender_encode(gender):
-  if gender==1:
-    return([1,0])
-  else:
-    return([0,1])
-
-# Method for encoding education value
-def education_encode(education):
-  if education==1:
-    return([0,1,0,0])
-  elif education==2:
-    return([0,0,1,0])
-  elif education==3:
-    return([1,0,0,0])
-  elif education==4:
-    return([0,0,0,1])
-
-# Method for encoding education value
-def marital_encode(marital_status):
-  if marital_status==1:
-    return([0,1,0])
-  elif marital_status==2:
-    return([1,0,0])
-  elif marital_status==3:
-    return([0,0,1])
-
-# Method for encoding repayment status values
-def rs_encode(rs):
-  if rs==-2:
-    return([1,0,0,0,0,0,0,0,0,0,0,0])
-  elif rs==-1:
-    return([0,1,0,0,0,0,0,0,0,0,0,0])
-  elif rs==0:
-    return([0,0,1,0,0,0,0,0,0,0,0,0])
-  elif rs==1:
-    return([0,0,0,1,0,0,0,0,0,0,0,0])
-  elif rs==2:
-    return([0,0,0,0,1,0,0,0,0,0,0,0])
-  elif rs==3:
-    return([0,0,0,0,0,1,0,0,0,0,0,0])
-  elif rs==4:
-    return([0,0,0,0,0,0,1,0,0,0,0,0])
-  elif rs==5:
-    return([0,0,0,0,0,0,0,1,0,0,0,0])
-  elif rs==6:
-    return([0,0,0,0,0,0,0,0,1,0,0,0])
-  elif rs==7:
-    return([0,0,0,0,0,0,0,0,0,1,0,0])
-  elif rs==8:
-    return([0,0,0,0,0,0,0,0,0,0,1,0])
-  elif rs==9:
-    return([0,0,0,0,0,0,0,0,0,0,0,1])
-  
-  
-
 app = Flask(__name__)
 def decompress_pickle(file):
   data = bz2.BZ2File(file, 'rb')
   data = pickle.load(data)
   return data
 
-model = decompress_pickle('model.pbz2')
+model = decompress_pickle('ccdp.pbz2')
 
 @app.route('/')
 def home():
@@ -78,17 +21,17 @@ def predict():
     For rendering results on HTML GUI
     '''
     if request.method == 'POST':
-        gender = gender_encode(int(request.form['gender']))
-        education = education_encode(int(request.form['education']))
-        marital_status = marital_encode(int(request.form['marriage']))
+        #gender = gender_encode(int(request.form['gender']))
+        #education = education_encode(int(request.form['education']))
+        #marital_status = marital_encode(int(request.form['marriage']))
         age = [int(request.form['age'])]
         bal_limit = [int(request.form['limit_bal'])]
-        rs_6 = rs_encode(int(request.form['april_rs']))
-        rs_5 = rs_encode(int(request.form['may_rs']))
-        rs_4 = rs_encode(int(request.form['june_rs']))
-        rs_3 = rs_encode(int(request.form['july_rs']))
-        rs_2 = rs_encode(int(request.form['august_rs']))
-        rs_1 = rs_encode(int(request.form['september_rs']))
+        rs_6 = [int(request.form['april_rs'])]
+        rs_5 = [int(request.form['may_rs'])]
+        rs_4 = [int(request.form['june_rs'])]
+        rs_3 = [int(request.form['july_rs'])]
+        rs_2 = [int(request.form['august_rs'])]
+        rs_1 = [int(request.form['september_rs'])]
         bill_6 = [int(request.form['bill_amt6'])]
         bill_5 = [int(request.form['bill_amt5'])]
         bill_4 = [int(request.form['bill_amt4'])]
@@ -102,12 +45,12 @@ def predict():
         pay_2 = [int(request.form['pay_amt2'])]
         pay_1 = [int(request.form['pay_amt1'])]
 
-
-    features = gender + education + marital_status + age + bal_limit
-    features = features + rs_6 + rs_5 + rs_4 + rs_3 + rs_2 + rs_1 
-    features = features + bill_6 + bill_5 + bill_4 + bill_3 + bill_2 + bill_1
-    features = features + pay_6 + pay_5 + pay_4 + pay_3 + pay_2 + pay_1
-
+    bill_amt_avg = [round(np.mean([bill_6, bill_5, bill_4, bill_3, bill_2, bill_1]), 2)]
+    features = rs_1 + rs_2 + pay_1 + bill_1
+    features = features + bal_limit + age + pay_2 + bill_2
+    features = features + pay_3 + bill_3 + bill_4 + pay_4
+    features = features + pay_6 + bill_5 + bill_6 + bill_amt_avg
+    
     features_arr = [np.array(features)]
 
     prediction = model.predict(features_arr)
